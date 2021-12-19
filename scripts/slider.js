7831b891e4dd706f9da60855b4f309014abebecb
Vitaliy-Finiuk <70633302+Vitaliy-Finiuk@users.noreply.github.com>
2021-12-19 22:05:12 +0200

    New "About me" and add "slider"

---
+++ scripts/slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    loop: true,
+
+    preloadImages: false,
+
+    lazy: {
+      loadOnTransitionStart: false,
+
+      loadPrevNext: false,
+    },
+
+    watchSlidesProgress: true,
+
+    watchSlidesVisibility: true,
+
+    breakpoints: {
+      320: {
+        slidersPerView: 1,
+      },
+      778: {
+        slidersPerView:2,
+      }
+    },
+  });
