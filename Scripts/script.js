80dff868a7aeab7307987645778a9fb1ed04d3cd
Vitaliy-Finiuk <70633302+Vitaliy-Finiuk@users.noreply.github.com>
2021-12-22 00:21:16 +0200

    New Header

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,6 @@
-const menuIcon = document.querySelector('.menu_icon');
+const menuIcon = document.querySelector('.menu-icon');
 if(menuIcon){
-    const menuBody = document.querySelector('.menu_body');
+    const menuBody = document.querySelector('.menu-body');
     menuIcon.addEventListener('click', function(e){
         document.body.classList.toggle('_lock');
         menuBody.classList.toggle('_active');
